9d840c9b434862fbb5478491799348bb18cb9d02
kaine <ghanirafli8@gmail.com>
2023-03-10 20:55:52 +0700

    basic horizontal movement enemy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { PlayerCharacter } from './player';
+import { Enemy } from './enemy';
 
 
 function App() {
   return (
     <div className="App">
       <PlayerCharacter cellSize={20} />
+      <Enemy cellSize={20} speed={10}/>
     </div>
   );
 }
--- src/enemy.tsx
+++ src/enemy.tsx
@@ -1,9 +1,68 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react";
 
-export const Enemy = () =>{
-    return(
-        <>
-        
-        </>
-    )
+interface EnemyProps {
+  cellSize: number;
+  areaWidth?: number;
+  areaHeight?: number;
+  center?: { x: number; y: number };
+  speed: number;
 }
+
+export const Enemy = ({
+  cellSize,
+  areaWidth = window.innerWidth,
+  center = {x: 100, y:100},
+  speed,
+}: EnemyProps) => {
+  const [enemyPos, setEnemyPos] = useState({ x: center.x, y: center.y });
+  const [moveDirection, setMoveDirection] = useState("right");
+  const enemyRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // calculate the new position based on the current direction of movement
+      const newPos = { x: enemyPos.x, y: enemyPos.y };
+      if (moveDirection === "left") {
+        newPos.x = Math.max(0, newPos.x - speed);
+        if (newPos.x === 0) {
+          setMoveDirection("right");
+        }
+      } else {
+        newPos.x = Math.min(areaWidth, newPos.x + speed);
+        if (newPos.x === areaWidth) {
+          setMoveDirection("left");
+        }
+      }
+  
+      // update the position
+      setEnemyPos(newPos);
+    }, 50); // change interval to whatever smoothness you desire
+  
+    return () => clearInterval(interval);
+  }, [enemyPos, moveDirection]);
+  
+
+  return (
+    <div
+      ref={enemyRef}
+      style={{
+        transition: "left 0.5s ease-out, top 0.5s ease-out",
+        position: "absolute",
+        left: `${enemyPos.x}px`,
+        top: `${enemyPos.y}px`,
+        width: `${cellSize}px`,
+        height: `${cellSize}px`,
+        backgroundColor: "blue",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: `${cellSize}px`,
+        fontWeight: "bold",
+        color: "white",
+        borderRadius: "50%",
+      }}
+    >
+      E
+    </div>
+  );
+};
